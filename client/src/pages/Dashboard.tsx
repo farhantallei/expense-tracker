6f01314326ed57d64855bfbe24c59520d7f6273c
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2022-09-29 20:58:18 +0700

    date changer

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -15,11 +15,14 @@ import {
   Text,
   useDisclosure,
 } from '@chakra-ui/react';
+import { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { InAndOutList, InAndOutModal } from '../components';
 import { useAuthContext } from '../context/AuthContext';
 
 function Dashboard() {
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1);
   const {
     isOpen: isInAndOutModalOpen,
     onOpen: onInAndOutModalOpen,
@@ -27,6 +30,23 @@ function Dashboard() {
   } = useDisclosure();
   const { account, setIsAuthenticated, setAccount } = useAuthContext();
   const navigate = useNavigate();
+  const totalWeek = useMemo((): number => {
+    if (currentMonth === new Date().getMonth() + 1) {
+      const currentDate = new Date().getDate();
+      return Math.ceil(currentDate / 7);
+    }
+    return Math.ceil(getDaysInMonth(currentYear, currentMonth) / 7);
+  }, [currentYear, currentMonth]);
+  const [currentWeek, setCurrentWeek] = useState(totalWeek);
+
+  useEffect(() => {
+    if (
+      currentYear === new Date().getFullYear() &&
+      currentMonth === new Date().getMonth() + 1
+    )
+      return setCurrentWeek(totalWeek);
+    setCurrentWeek(1);
+  }, [currentYear, currentMonth]);
 
   function logout() {
     setAccount(null);
@@ -57,15 +77,33 @@ function Dashboard() {
         marginTop={8}
         alignItems="center">
         <IconButton
+          onClick={() => {
+            setCurrentMonth((prevMonth) => {
+              if (prevMonth > 0) return prevMonth - 1;
+              setCurrentYear((prevYear) => prevYear - 1);
+              return 11;
+            });
+          }}
           size="sm"
           variant="outline"
           aria-label="Previous month"
           icon={<ChevronLeftIcon />}
         />
         <Heading size="md" justifySelf="center" fontWeight="semibold">
-          Agustus/2022
+          {formatDatePagination(new Date(currentYear, currentMonth, 0))}
         </Heading>
         <IconButton
+          onClick={() => {
+            setCurrentMonth((prevMonth) => {
+              if (prevMonth < 12) return prevMonth + 1;
+              setCurrentYear((prevYear) => prevYear + 1);
+              return 0;
+            });
+          }}
+          disabled={
+            currentYear === new Date().getFullYear() &&
+            currentMonth === new Date().getMonth() + 1
+          }
           size="sm"
           variant="outline"
           aria-label="Next month"
@@ -78,11 +116,13 @@ function Dashboard() {
           Week
         </Text>
         <ButtonGroup size="sm" isAttached variant="outline">
-          <Button isActive>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button>4</Button>
-          <Button>5</Button>
+          {[...Array(totalWeek)].map((x, i) => (
+            <Button
+              isActive={i + 1 === currentWeek}
+              onClick={() => setCurrentWeek(i + 1)}>
+              {i + 1}
+            </Button>
+          ))}
         </ButtonGroup>
       </Flex>
       {/* Stat */}
@@ -144,6 +184,17 @@ function Dashboard() {
       />
     </Container>
   );
+}
+
+function formatDatePagination(date: Date) {
+  return new Intl.DateTimeFormat(undefined, {
+    month: 'long',
+    year: 'numeric',
+  }).format(date);
+}
+
+function getDaysInMonth(year: number, month: number) {
+  return new Date(year, month, 0).getDate();
 }
 
 export default Dashboard;
